9cbc79cc7030857428d62c1d7a1f00be503275ad
Jaspel Bosales <79386121+bjaspel63@users.noreply.github.com>
2025-08-09 23:11:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -492,7 +492,7 @@ function closeSession() {
     video.srcObject = null;
   }
   
-  const roomNameDisplay = document.getElementById("roomNameDisplay");
+
   if (roomNameDisplay) {
     roomNameDisplay.textContent = ""; // clear text
     roomNameDisplay.style.display = "none"; // optionally hide
